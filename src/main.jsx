41401870da00959625748658e0200597e4bcdc2a
ginanjargigin <ginanjargigin33@gmail.com>
2026-09-03 19:57:24 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import LoginGate from "./LoginGate.jsx";
 import "./App.css";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <App />
+    <LoginGate>
+      <App />
+    </LoginGate>
   </React.StrictMode>
 );
